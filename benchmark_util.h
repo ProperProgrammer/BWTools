f89be039291d151cec3c33bee91a7de81b158c66
James Thompson <james@bitwizestudio.com>
2013-10-21 21:39:48 -0700

    Updated to the latest benchmark_utils.h -- this header is all over the place.

--- benchmark_util.h
+++ benchmark_util.h
@@ -1,3 +1,4 @@
+//
 // The MIT License (MIT)
 //
 // Copyright (c) 2013 James Thompson
@@ -24,13 +25,17 @@
 
 static double _benchmarkDelta;
 
+typedef void (^benchmark_block_t)(void);
+
 static inline double get_time()
 {
     struct timeval t;
-    struct timezone tzp;
-    gettimeofday(&t, &tzp);
+    gettimeofday(&t, NULL);
     return t.tv_sec + t.tv_usec * 1e-6;
 }
+
+// This compiler flag gives some smarts to the analyzer to pick up on format errors.
+static inline void benchmark_timer_start(NSString *str, ...) NS_FORMAT_FUNCTION(1, 2);
 
 static inline void benchmark_timer_start(NSString *str, ...)
 {
@@ -44,6 +49,9 @@ static inline void benchmark_timer_start(NSString *str, ...)
     va_end(args);
 }
 
+// This compiler flag gives some smarts to the analyzer to pick up on format errors.
+static inline void benchmark_timer_stop(NSString *str, ...) NS_FORMAT_FUNCTION(1, 2);
+
 static inline void benchmark_timer_stop(NSString *str, ...)
 {
     _benchmarkDelta = get_time() - _benchmarkDelta;
@@ -53,5 +61,68 @@ static inline void benchmark_timer_stop(NSString *str, ...)
     va_list args;
     va_start(args, str);
     NSLogv(str, args);
+    va_end(args);
+}
+
+static inline void benchmark_block(benchmark_block_t benchmarkBlock, NSString *logString, ...) NS_FORMAT_FUNCTION(2, 3);
+
+static inline void benchmark_block(benchmark_block_t benchmarkBlock, NSString *logString, ...)
+{
+    if (benchmarkBlock == NULL)
+        return;
+    
+    NSString *startLogString = [NSString stringWithFormat:@"%@ 🚩 Benchmark Timer Began 🚩", (logString) ? logString : @""];
+    
+    va_list args;
+    
+    va_start(args, logString);
+    NSLogv(startLogString, args);
+    va_end(args);
+    
+    double dt = get_time();
+    
+    benchmarkBlock();
+    
+    dt = get_time() - dt;
+    
+    NSString *endLogString = [NSString stringWithFormat:@"%@ ⏰ Completed In %f Seconds ⏰", logString, dt];
+    
+    va_start(args, logString);
+    NSLogv(endLogString, args);
+    va_end(args);
+}
+
+static inline void benchmark_block_avg(benchmark_block_t benchmarkBlock, int numIterations, NSString *logString, ...) NS_FORMAT_FUNCTION(3, 4);
+
+static inline void benchmark_block_avg(benchmark_block_t benchmarkBlock, int numIterations, NSString *logString, ...)
+{
+    if (benchmarkBlock == NULL)
+        return;
+    
+    NSString *startLogString = [NSString stringWithFormat:@"%@ 🚩 Benchmark Timer Began. Performing %d Iterations. 🚩", (logString) ? logString : @"", numIterations];
+    
+    va_list args;
+    
+    va_start(args, logString);
+    NSLogv(startLogString, args);
+    va_end(args);
+    
+    double totalDelta = 0;
+    
+    for (int i = 0; i < numIterations; i ++)
+    {
+        double curDelta = get_time();
+        
+        benchmarkBlock();
+        
+        totalDelta += get_time() - curDelta;
+    }
+    
+    double dt = totalDelta / numIterations;
+    
+    NSString *endLogString = [NSString stringWithFormat:@"%@ ⏰ Mean Execution Time Was: %f Seconds ⏰", logString, dt];
+    
+    va_start(args, logString);
+    NSLogv(endLogString, args);
     va_end(args);
 }
